4d34d98cdc6f08e5ae8d34081065f1d44ea81ffb
timoncraus <tima.gorshkov@list.ru>
2022-08-04 20:25:46 +0300

    edit file

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("000");
     }
 
     // Update is called once per frame
